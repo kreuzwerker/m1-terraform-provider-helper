2bca57b31c9cfaadabdb19a7a3e1e2047957820d
Martin Wentzel <martin.wentzel@kreuzwerker.de>
2022-07-21 12:21:30 +0200

    feat: Implement logrus logging.

--- cmd/root.go
+++ cmd/root.go
@@ -13,22 +13,23 @@ import (
 // from the main func. All other sub-commands should
 // be registered here.
 func RootCmd() *cobra.Command {
-	var v string
-
 	cmd := &cobra.Command{
 		Use:   "m1-terraform-provider-helper",
 		Short: "A CLI to manage the installation of terraform providers for the Mac M1 chip",
 	}
 
 	cmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
-		if err := setUpLogs(os.Stdout, v); err != nil {
+		value, ok := os.LookupEnv("TF_HELPER_LOG")
+		if !ok {
+			value = logrus.WarnLevel.String()
+		}
+
+		if err := setUpLogs(os.Stdout, value); err != nil {
 			return err
 		}
 
 		return nil
 	}
-
-	cmd.PersistentFlags().StringVarP(&v, "verbosity", "v", logrus.WarnLevel.String(), "Log level (debug, info, warn, error, fatal, panic")
 
 	cmd.AddCommand(
 		statusCmd(),
